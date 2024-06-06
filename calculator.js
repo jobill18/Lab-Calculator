fa176c0907879f4a49507f2feab14144c4178a38
Joseph Billings <josephbillings.gb@gmail.com>
2024-06-06 14:05:59 -0600

    First attempt at tokens with functions nested

--- calculator.js
+++ calculator.js
@@ -16,12 +16,28 @@ function power(num1, num2) {
 function mod(num1, num2) {
   return num1 % num2;
 }
-function sqrt(num) {
-  return num ** 0.5;
+function sqrt(num1) {
+  return num1 ** 0.5;
 }
 
 
-function calculate(expression) {}
+function calculate(expression) {
+  const tokens = expression.split(' ');
+  if (token[1] === "sqrt") {
+  sqrt(token[2]);
+  } else if (token[2] === '+'){
+    add(token[1],token[2];)
+  } else if (token[2] === '-'){
+    add(token[1],token[2];)
+  } else if (token[2] === '*'){
+    multiply(token[1],token[2];)
+  } else if (token[2] === '/'){
+    divide(token[1],token[2];)
+  } else if (token[2] === '^'){
+    power(token[1],token[2];)
+  } else if (token[2] === '%'){
+    mod(token[1],token[2];)
+}
 
 
 
